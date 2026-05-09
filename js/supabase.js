5eef5d626de2dbead93024dd73f293ab4b983398
Promise-cpu <offorp211@gmail.com>
2026-05-09 23:48:47 +0100

    initial commit

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const supabaseUrl = 'https://tabdzfwojeibimaxumex.supabase.co'
+const supabaseKey = 'sb_publishable_JFdYPuCwiAgcI2eZ7Cxt2w_i68ppAHy'
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+)
